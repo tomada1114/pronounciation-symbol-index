2ede5e0e5a0a35b2839a3008b3b2e5f48a1abf54
tomada <tmasuyama1114@gmail.com>
2026-02-05 08:27:33 -0600

    chore: configure Serwist PWA with webpack support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+import withSerwistInit from '@serwist/next'
 import type { NextConfig } from 'next'
+
+const withSerwist = withSerwistInit({
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js',
+})
 
 const nextConfig: NextConfig = {
   /* config options here */
 }
 
-export default nextConfig
+export default withSerwist(nextConfig)
