9a709b481aa010a026781671e6407da3abab68ef
tomada <tmasuyama1114@gmail.com>
2026-02-04 19:29:21 -0600

    style: add swipe-container CSS and IntersectionObserver mock for tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom/vitest'
+
+global.IntersectionObserver = class IntersectionObserver {
+  readonly root: Element | null = null
+  readonly rootMargin: string = ''
+  readonly thresholds: ReadonlyArray<number> = []
+  constructor(
+    _callback: IntersectionObserverCallback,
+    _options?: IntersectionObserverInit,
+  ) {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+  takeRecords(): IntersectionObserverEntry[] {
+    return []
+  }
+}
